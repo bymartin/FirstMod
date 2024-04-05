1e8023b6e599fd9d99580299294e50557a145b3e
Barry Martin <bymartin@users.noreply.github.com>
2024-04-05 18:25:19 -0500

    Add custom item group

--- src/main/java/net/barry/firstmod/FirstMod.java
+++ src/main/java/net/barry/firstmod/FirstMod.java
@@ -3,11 +3,12 @@ package net.barry.firstmod;
 import net.barry.firstmod.items.Spear;
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.FoodComponents;
-import net.minecraft.item.Item;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,15 +18,32 @@ public class FirstMod implements ModInitializer {
 	public static final Item BANANA = new Item(new Item.Settings().maxCount(20).food(FoodComponents.BEEF)); // or new FabricItemSettings()
 	public static final Item SPEAR = new Spear(new Item.Settings().maxCount(1));
 
+	// custom item group
+	public static final ItemGroup WEAPONS = Registry.register(Registries.ITEM_GROUP,
+			new Identifier(MOD_ID, "weapons_group"),
+			FabricItemGroup.builder()
+					.displayName(Text.of("Weapons"))
+					.noScrollbar()
+					.icon(() -> new ItemStack(Items.CHIPPED_ANVIL)).entries((displayContext, entries) -> {
+						entries.add(SPEAR);
+						entries.add(BANANA);
+					}).build());
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world from firstmod!");
+
+		// Add to a vanilla group
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+			entries.add(BANANA);
+		});
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+			entries.addAfter(Items.IRON_SWORD, SPEAR);
+		});
+
 		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "banana"), BANANA);
 		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "spear"), SPEAR);
 	}
